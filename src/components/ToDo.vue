c73667e578d7408dab2f8421d7398f102b3b7026
Sven Ott <sven.ott@methodpark.de>
2019-06-24 11:35:24 +0200

    - Style geändert, Animation für alertmsg - delete-Funktion für einzelne Elemente implementiert - bugfix bzgl leerem Userinput implementiert
    definitiv feierbar

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="hello">
-    <div class="holder">
-<form @submit.prevent="addToArray(todo)">
-  <input type="text" placeholder="Enter a task for the day" v-model="todo">
-  <input type="checkbox" id="checkbox" v-model="checked">
-</form>
-  <button @click="clear()">Clear List</button>
-<ul>
-  <li v-for="(data, index) in todos" v-bind:key='index'>  
-    {{ index + 1 }}. {{ data }}
-  </li>
+  <div class="holder">
+    <form @submit.prevent="addToArray(todo)">
+      <input type="text" placeholder="Enter a task for the day" v-model="todo" v-validate="'min:3'" name="todo">    
+      <transition name="alert-in">
 
+      <p class="alert" v-if="errors.has('todo')">{{errors.first('todo')}}</p>
+      </transition>
+
+   </form>
   
+   <ul>
+      <li v-for="(tro, index) in todos" v-bind:key='index'>  
+       {{ index + 1 }}. {{ tro }}
+         <i  class = "fa fa-minus-circle" v-on:click="remove(index)">                    
+        </i>
+      
+      </li>
 
-  <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }">
-    {{ todo }}
-  </div>
+   <button @click="clear()">Clear List</button>
 
   <p v-if="isEmpty">You have things to do!</p>
   <p v-else>You have nothing to do.</p>
@@ -27,37 +30,45 @@
     
 </template>
 
+
 <script>
-
-
 
 export default {
   name: 'ToDo',
   data() {
     return {
-      checked: false,
+      
       todo: '',
-      todos: ["Beispiel ToDo"],
-      bgColor: 'yellow',
-      bgWidth: '100%',
-      bgHeight: '100px'
+      todos: ["Finish this website", "Pizza Rotolo"],
+      
     }
   }, 
   methods: {
     addToArray(todo) {
-      this.todos.push(todo);
-      this.todo = "";
       
+        if (todo.length >= 3) {
+          this.todos.push(todo);
+          this.todo = "";
+        } else {
+          console.log('Not valid');
+        
+      }
+      
+    
       },
     clear() {
       this.todos = [];
+    },
+    remove(id) {
+      this.todos.splice(id,1);
+    
     }
     },
   computed: {
     isEmpty: {
       get: function()
       {
-        return this.todos.length - 1 >= 0;
+        return this.todos.length > 0;
       }
     },
       
@@ -69,18 +80,92 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
+.holder {
+    background: #fff;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+.holder {
+  display: flex;
+  justify-content: center;
+  align-items: left;
+  align-content: center;
+  flex-direction: column;
   
-  margin: 10px;
-  text-align: left;
-  margin-left: 40%;
+}
+  button {
+    width: calc(60 - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #d3e7e771;
+    color: #687F7F;
+    margin: auto;
+    
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
   
-}
-#li1 {
-  margin-bottom: 10px;
-}
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  i{
+    float: right;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s
+
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
 </style>
